5b5a26eef44ab451097c12df86c4ed6bec6e36ee
Jaya p <59704700+jayaputra212@users.noreply.github.com>
2024-04-20 13:43:24 +0700

    Update ehcp-install.sh

--- ehcp-install.sh
+++ ehcp-install.sh
@@ -2,6 +2,7 @@
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe -y
 sudo apt update
+sudo apt-get install apache2 mariadb-server apache2 php7.2 php7.2-gd php7.2-json php7.2-mysql php7.2-curl php7.2-mbstring php7.2-intl php7.2-imagick php7.2-xml php7.2-zip libapache2-mod-php7.2 unzip wget -y
 sudo apt-get install wget subversion curl -y
 wget -O "ehcpforce_stable_snapshot.tar.gz" -N https://github.com/jayaputra212/ehcp/blob/main/ehcpforce_stable_snapshot.tar.gz
 tar -zxvf "ehcpforce_stable_snapshot.tar.gz"
